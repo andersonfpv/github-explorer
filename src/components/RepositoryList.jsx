c265bae6ae360ddb87ab9270367540ad90792992
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-09-30 17:01:45 -0300

    iniciando useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocktseat/repos
 
 const repository = {
     name: 'unform',
@@ -9,6 +12,7 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
